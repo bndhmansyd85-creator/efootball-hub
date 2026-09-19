cc2d03e30de0d71223adaf3704dc8bc54fbb791b
bndhmansyd85-creator <bndhmansyd85@gmail.com>
2026-09-19 08:51:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // eFootball Hub Auto Backend
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const cheerio = require("cheerio");
 
@@ -7,6 +8,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 // دالة جلب الأخبار من موقع كونامي
 async function fetchKonamiNews() {
